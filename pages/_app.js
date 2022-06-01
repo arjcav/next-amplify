a294fe37ef0c4d181a68a4760764ec5ab55d91a8
arjcav <acastro@arkusnexus.com>
2022-06-01 03:09:18 -0400

    final eslint ,prettier config

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import React from 'react'
 import '../styles/globals.css'
 
 // eslint-disable-next-line react/prop-types
-function MyApp ({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
-export default function Home () {
+export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hi Next Amplify</h1>
     </div>
-  )
+  );
 }
